6530b67eecd4cd7d8991820c1b7841b96c8e9369
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-04 19:52:40 +0300

    feat(product): make routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import HomePage from "./components/Home/HomePage";
+import AboutPage from "./components/About/AboutPage";
+import ProjectsPage from "./components/Projects/ProjectsPage";
+import ContactsPage from "./components/Contacts/ContactsPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+    (
+        <Router>
+            <Switch>
+            <Route exact path='/'>
+                <HomePage />
+            </Route>
+                <Route path='/about'>
+
+                    <AboutPage />
+
+                </Route>
+                <Route path='/projects'>
+
+                    <ProjectsPage />
+
+                </Route>
+                <Route path='/contacts'>
+
+                    <ContactsPage />
+
+                </Route>
+            </Switch>
+        </Router>
+    );
 
 export default App;
