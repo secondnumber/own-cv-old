ad651a074373fb92bf216f79fc4e2fa3c6117c67
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-05 22:38:01 +0300

    feat(product): make home page

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import classes from './HomePage.module.scss'
+import Logo from '../../assets/img/logo.png'
+import ToggleButtons from "../common/ToggleButtons";
 
-const HomePage = () => <div>HomePage</div>;
+const HomePage = () =>
+    <div className={classes.wrapper}>
+            <div className={classes.introBlock}>
+                <div className={classes.intro}>
+                    <img className={classes.logo} src={Logo} alt="Logo"/>
+                        <p className={classes.title}>
+                            <span className={classes.turquoise}>&lt;title&gt;</span>
+                            &nbsp;Hello&nbsp;
+                            <span className={classes.turquoise}>&lt;/title&gt;</span>
+                        </p>
+                        <h1 className={classes.title}>
+                            <span className={classes.turquoise}>&lt;h1&gt;</span>
+                            &nbsp;I am <span className={classes.bold}>Ana Doe&nbsp;</span>
+                            <span className={classes.turquoise}>&lt;/h1&gt;</span>
+                        </h1>
+                        <p className={classes.subtitle}>
+                            <span className={classes.fuchsia}>&lt;h2&gt;</span>
+                            &nbsp;A <span className={classes.bold}>HTML5 </span>CODER&nbsp;
+                            <span className={classes.fuchsia}>&lt;/h2&gt;</span>
+                        </p>
+                        <p className={classes.caption}>
+                            <span className={classes.turquoise}>&#123;</span>
+                            &nbsp;Design & Code&nbsp;
+                            <span className={classes.turquoise}>&#125;</span>
+                        </p>
+                        <p className={classes.attribute}>CLASS =&nbsp;
+                            <span className={`${classes.fuchsia} ${classes.bold} ${classes.big}`}>NINJA</span>
+                        </p>
+                    </div>
+                </div>
+            <div className={classes.imageBlock}>
+                <div className={classes.buttons}>
+                <ToggleButtons />
+                </div>
+            </div>
+    </div>;
 
 export default HomePage;
