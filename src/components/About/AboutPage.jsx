1bb1b2027f499ce0e0ac7014805142e07de8eaa5
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-05 23:01:08 +0300

    feat(product): start about page

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import classes from './AboutPage.module.scss'
+import Logo from '../../assets/img/logo.png'
+import ToggleButtons from "../common/ToggleButtons/ToggleButtons";
+import Button from "../common/Button/Button";
 
-const AboutPage = () => <div>AboutPage</div>;
+const AboutPage = () =>
+    <div className={classes.wrapper}>
+        <div className={classes.toggles}>
+            <ToggleButtons />
+        </div>
+        <div className={classes.block}>
+            <div className={classes.logo}>
+                <img className={classes.logo} src={Logo} alt="Logo"/>
+            </div>
+            <h1>
+                <span className={classes.turquoise}>&#123;</span>
+                &nbsp;About Me&nbsp;
+                <span className={classes.turquoise}>&#125;</span>
+            </h1>
+            <h2 className={classes.caption}>I’m Front-end Developer with
+                6 years experience</h2>
+            <p className={classes.description}>
+                &lt;p&gt; Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                    parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+                    pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. &lt;/p&gt;
+            </p>
+            <Button />
+            <div className={classes.achievements}>
+
+            </div>
+        </div>
+    </div>;
 
 export default AboutPage;
---
+++ src/components/common/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from './Button.module.scss'
+
+const Button = () =>
+    <button>Button</button>;
+
+export default Button;
