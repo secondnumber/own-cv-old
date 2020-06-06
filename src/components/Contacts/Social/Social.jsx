e76070e8dd7b606b74110fcaebde4fa59ae49401
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-06 18:46:39 +0300

    feat(product): make contacts page

---
+++ src/components/Contacts/Social/Social.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Social.module.scss';
+import FacebookImg from '../../../assets/img/facebook.png';
+
+const Social = (props) =>
+    <li className={classes.item}>
+        <img src={FacebookImg} alt="Facebook"/>
+    </li>;
+
+export default Social;
